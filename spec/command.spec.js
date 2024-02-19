60cd5e7031657aadd13ceee4d2d45389e515dbb6
stacijbrooks <stacibrooksfitness@gmail.com>
2024-02-18 21:04:36 -0600

    Mars Rover 2.18.24

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -8,5 +8,11 @@ describe("Command class", function() {
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
+  it("constructor sets command type", function() {
+    let commandType = "TEST_COMMAND";
+    let command = new Command(commandType);
+    expect(command.commandType).toEqual(commandType);
+  });
 
 });
+
