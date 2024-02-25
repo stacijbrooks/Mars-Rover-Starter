fb3bc070645d59443d3f49974851c8fb139f93ec
stacijbrooks <stacibrooksfitness@gmail.com>
2024-02-25 13:07:23 -0600

    Mars Rover Updated

--- rover.js
+++ rover.js
@@ -12,43 +12,40 @@ class Rover {
    for (let command of commands) {
 // Handle different command types
    switch (command.commandType) {
-      case 'MOVE':
-         // Check if the rover is in LOW_POWER mode
-         if (this.mode === 'LOW_POWER') {
-            // If in LOW_POWER mode, the command is not completed
-            results.push({ completed: false });
-         } else {
-            // If in NORMAL mode, update rover's position and mark the command as completed
-            this.position = command.value;
-            results.push({ completed: true });
-         }
-         break;
+   case 'MOVE':
+   // Check if the rover is in LOW_POWER mode
+   if (this.mode === 'LOW_POWER') {
+      // If in LOW_POWER mode, the command is not completed
+      results.push({ completed: false });
+   } else {
+      // If in NORMAL mode, update rover's position and mark the command as completed
+      this.position = command.value;
+      results.push({ completed: true });
+   }
+   break;
 
-      case 'STATUS_CHECK':
-         // Return the current status of the rover
-         results.push({
-            completed: true,
-            roverStatus: {
-               mode: this.mode,
-               generatorWatts: this.generatorWatts,
-               position: this.position
-            }
-         });
-         break;
+   case 'STATUS_CHECK':
+   // Return the current status of the rover
+   results.push({ completed: true, roverStatus: {
+         mode: this.mode,
+         generatorWatts: this.generatorWatts,
+         position: this.position
+      }
+   });
+   break;
 
-      case 'MODE_CHANGE':
-         // Change the rover's mode to the specified mode
-         this.mode = command.value;
-         // Mark the command as completed
-         results.push({ completed: true });
-         break;
+   case 'MODE_CHANGE':
+      // Change the rover's mode to the specified mode
+      this.mode = command.value;
+      // Mark the command as completed
+      results.push({ completed: true });
+      break;
 
-      default:
-         // If the command type is unknown, mark the command as not completed
-         results.push({ completed: false });
+   default:
+      // If the command type is unknown, mark the command as not completed
+      results.push({ completed: false });
    }
 }
-
 // Return an object containing the name of the message and the results of executing the commands
 return { message: name, results };
    }
