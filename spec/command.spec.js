e8137092d54d0b7853c8f641e246fd0c0cb388c5
stacijbrooks <stacibrooksfitness@gmail.com>
2024-02-24 21:04:42 -0600

    Update command.spec.js

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -8,11 +8,15 @@ describe("Command class", function() {
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
-  it("constructor sets command type", function() {
-    let commandType = "TEST_COMMAND";
-    let command = new Command(commandType);
-    expect(command.commandType).toEqual(commandType);
+  it('constructor sets command type', function() {
+    let command = new Command('MOVE', 12000);
+    expect(command.commandType).toEqual('MOVE');
+  });
+  it('constructor sets a value passed in as the 2nd argument', function() {
+    let command = new Command('MOVE', 12000);
+    expect(command.value).toEqual(12000);
   });
 
-});
+  });
+
 
